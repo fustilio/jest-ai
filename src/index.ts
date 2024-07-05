a41ad362574d3940ee8835a94c71f86378ce738d
Francis Lee <francis@u.nus.edu>
2024-07-05 13:05:52 +0800

    chore: export matchers

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import * as extensions from './matchers'
 import { expect } from '@jest/globals';
 
+export const matchers = extensions;
+
 expect.extend(extensions)
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,1 +1,18 @@
 /// <reference path="jest.d.ts" />
+
+import type { ExpectationResult, AsyncExpectationResult, MatcherUtils } from 'expect'
+import type { ChatCompletion } from "openai/src/resources/chat/completions";
+import type { RequiredActionFunctionToolCall, Run } from "openai/src/resources/beta/threads/runs";
+import type { z } from 'zod'
+
+declare module 'jest-ai' {
+    declare const matchers: {
+        toHaveUsedAllAssistantTools(received: () => Promise<ChatCompletion>, expectedTools: string[]): AsyncExpectationResult;
+        toHaveUsedAllTools(received: () => Promise<ChatCompletion>, expectedTools: string[]): AsyncExpectationResult;
+        toHaveUsedSomeAssistantTools(received: Run, expectedTools: string[] | RequiredActionFunctionToolCall.Function[]): AsyncExpectationResult;
+        toHaveUsedSomeTools(received: () => Promise<ChatCompletion>, expectedTools: string[]): AsyncExpectationResult;
+        toMatchZodSchema(received: z.Schema<any, any>, expected: string): ExpectationResult;
+        toSatisfyStatement(received: string, expected: string): AsyncExpectationResult;
+        toSemanticallyMatch(received: string, expected: string): AsyncExpectationResult;
+    };
+}
